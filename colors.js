adda6ab3a2734a5c497c186c0508d1bb462d21c8
whdms2008 <45191177+whdms2008@users.noreply.github.com>
2020-01-21 15:44:38 +0900

    Update colors.js

--- colors.js
+++ colors.js
@@ -77,11 +77,19 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 })();
 
 //밤낮 시스템
-var d = new Date();
+// var d = new Date();
+// var h = d.getUTCHours()-9;
+// if (h < 7){
+//     document.querySelector("#moon").style.top = 110-13*h;  //11시간 
+// } else {
+//     document.querySelector("#moon").style.top = 110-13*7+13*(h-7);  //11시간 
+//     }
+// document.querySelector("#moon").style.right = 1585-130*h;var d = new Date();
 var h = d.getUTCHours()-9;
-if (h < 7){
+if (true){
     document.querySelector("#moon").style.top = 110-13*4;  //11시간 
 } else {
     document.querySelector("#moon").style.top = 110-13*7+13*(4-7);  //11시간 
     }
 document.querySelector("#moon").style.right = 1585-130*4;
+
